045f8df7f6e5ca4eac5bc659b5cee3f83567aa51
Ali-Rawe <Alra0059@algonquinlive.com>
2022-01-21 14:03:37 -0500

    step-2

---
+++ app/src/main/java/com/cst2335/lab2/MainActivity.java
@@ -0,0 +1,53 @@
+package com.cst2335.lab2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    Switch sw;
+    Snackbar sb;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_grid);
+
+
+        btn = findViewById(R.id.mybtn);
+        sw = findViewById(R.id.switch2);
+        sw.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton cb, boolean b) {
+
+                String onoff = "";
+
+                if (b){
+                    onoff = getString(R.string.on);
+                }else{
+                    onoff = getString(R.string.off);
+                }
+                Snackbar mySnackbar = Snackbar.make(findViewById(R.id.content),
+                        getString(R.string.switch_text)+ onoff, Snackbar.LENGTH_SHORT);
+                mySnackbar.setAction(R.string.undo_string, click -> cb.setChecked(!b));
+                mySnackbar.show();
+            }
+        });
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "This is toast message.", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
