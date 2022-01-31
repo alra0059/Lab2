fd8b6390afb3413c0e403f975db1760766f2dc8e
Ali-Rawe <Alra0059@algonquinlive.com>
2022-01-31 12:17:05 -0500

    step-3

--- app/src/main/java/com/cst2335/lab2/MainActivity.java
+++ app/src/main/java/com/cst2335/lab2/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_grid);
+        setContentView(R.layout.activity_main_relative);
 
 
         btn = findViewById(R.id.mybtn);
